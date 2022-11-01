fffca3ea745186af1c08f243356425d698176e67
julyy5 <smirnova-1999@inbox.ru>
2022-11-01 11:00:37 +0300

    JSON Schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,30 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.Test;
 
+import java.net.http.HttpResponse;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1/demo/accounts")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+
+
     }
+
+
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -3,15 +3,17 @@ package ru.netology.rest;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV6 {
     private RequestSpecification requestSpec = new RequestSpecBuilder()
         .setBaseUri("http://localhost")
-        .setBasePath("/api/v1")
+        .setBasePath("/api/v1/demo/accounts")
         .setPort(9999)
         .setAccept(ContentType.JSON)
         .setContentType(ContentType.JSON)
@@ -30,5 +32,8 @@ class MobileBankApiTestV6 {
       // Проверки
       .then()
           .statusCode(200);
+
     }
+
+
 }
